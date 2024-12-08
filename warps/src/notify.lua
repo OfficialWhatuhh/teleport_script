52475b35613cb00afa3162320f126ba0f6e06fe3
OfficialWhatuhh <kudzucouhh@gmail.com>
2024-12-08 00:24:03 -0500

    Update notify.lua

--- warps/src/notify.lua
+++ warps/src/notify.lua
@@ -1,5 +1,4 @@
--- https://runtime.fivem.net/doc/natives/?_0x2ED7843F8F801023
--- this is the new notification method
+
 function notify(str)
     BeginTextCommandThefeedPost("STRING")
     AddTextComponentSubstringPlayerName(str)
